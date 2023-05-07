74738b488b5efdade637ff6b5af8593aec7af263
littleboycat <75587667+littleboycat@users.noreply.github.com>
2023-05-07 15:24:33 -0700

    Update build1.py

--- build1.py
+++ build1.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import ttk, filedialog
 from tkinter.filedialog import askopenfile
 from PIL import Image, ImageDraw, ImageTk
+import math
+from random import randint
 
 class Application(Frame):
     def __init__(self, master=None):
@@ -10,25 +12,56 @@ class Application(Frame):
         self.canvas = None
         self.image_tk = None
         self.img_dim = None
-        self.calibration = [] # keep track of calibration coordinates
         self.mode = 0
 
+        self.calibration = [] # keep track of calibration coordinates
         self.ent_calibrate = None
+        self.btn_ok = None
+        self.drop = None
         self.var = StringVar(self, value="mm")
-        self.scale = None
-        self.DX = 0
+        self.DX = None
 
         self.pack()
         self.create_widgets()
 
     def calibrate(self):
         self.calibration = []
-        print(self.scale)  #################################################################################
-        print(len(self.calibration) == 2)
-        if len(self.calibration) == 2:
-            self.DX = self.scale/self.distance()
-            print(self.DX)
+        self.mode = 1
+        self.toggle_to("normal")
+        self.canvas.delete("calibration")
+        notice1 = Label(text="click 2 points to calibrate", name="notice1").place(x=0,y=self.img_dim[1]+40)
 
+    def generate(self):
+        # define bounding box x/3 and y/3
+        bx = self.img_dim[0] // 3
+        by = self.img_dim[1] // 3
+
+        # pick point in bound box
+        p = (randint(bx,2*bx), randint(by,by*2))
+
+        # random angle and opposite angle for bounding circle around point
+        o1 = randint(0,360)*math.pi/180
+        # 
+        r = min([bx,by])
+        end1x = p[0] + round(r*math.cos(o1))
+        end1y = p[1] + round(r*math.sin(o1))
+        end2x = p[0] - round(r*math.cos(o1))
+        end2y = p[1] - round(r*math.sin(o1))
+
+        # display new line and delete old line
+
+        self.canvas.delete("line")
+        self.canvas.create_line(end1x, end1y, end2x, end2y, fill="yellow", tags="line", width=2)
+
+        
+
+    def toggle_to(self, to):
+        self.ent_calibrate.config(state=to)
+        self.btn_ok.config(state=to)
+        self.drop.config(state=to)
+
+        if len(self.calibration) != 2:
+            self.btn_ok.config(state="disabled")
 
 
 
@@ -67,29 +100,38 @@ class Application(Frame):
 
         wid = self.img_dim[0]
         height = self.img_dim[1]
-        # Display the image in a Tkinter window
+
+        # set dimensions of frame
         self.master.geometry("{}x{}".format(wid+150,height+75))
 
+        # initialize buttons for UI
         btn_calibrate = Button(text="Calibrate",width=10,height=2, bg="white", fg="black", command=self.calibrate)
         btn_calibrate.place(x=wid,y=0)
 
-        btn_generate = Button(text="Generate",width=10,height=2, bg="white", fg="black", justify="center")
+        btn_generate = Button(text="Generate",width=10,height=2, bg="white", fg="black", command=self.generate)
         btn_generate.place(x=wid,y=50)
 
         btn_calculate = Button(text="Calculate",width=10,height=2, bg="white", fg="black", justify="center")
         btn_calculate.place(x=wid,y=100)
 
-        self.ent_calibrate = Entry(bg="black",fg="white", width=10)
-        self.ent_calibrate.place(x=0,y=height)
+        self.ent_calibrate = Entry(bg="#D3D3D3",fg="black", insertbackground="black", width=10)
+        self.ent_calibrate.place(x=0,y=height+10)
 
-        btn_ok = Button(text="OK",width=2,height=2, command=self.ok)
-        btn_ok.place(x=200,y=height)
+        self.btn_ok = Button(text="OK",width=2,height=2, command=self.ok)
+        self.btn_ok.place(x=200,y=height+10)
 
-        drop = OptionMenu(self.master, self.var, "mm", "um", "nm", "pm")
-        drop.place(x=125,y=height)
+        self.drop = OptionMenu(self.master, self.var, "mm", u'\u03BC'+"m", "nm", "pm")
+        self.drop.place(x=125,y=height+10)
+
+        # gray out buttons until calibration done
+        self.toggle_to("disabled")
+
 
     def ok(self):
-        self.scale = self.magnitude()
+        scale = self.magnitude()
+        self.DX = scale/self.distance(self.calibration[0], self.calibration[1])
+        self.toggle_to("disabled")
+        # self.delete('notice1')
 
     def magnitude(self):
         dist = 0
@@ -99,7 +141,7 @@ class Application(Frame):
 
         if v == "mm":
             return dist/(10**3)
-        elif v == "um":
+        elif v == u'\u03BC'+"m":
             return dist/(10**6)
         elif v == "nm":
             return dist/(10**9)
@@ -119,20 +161,22 @@ class Application(Frame):
         # Get the coordinates of the mouse click
         x, y = event.x, event.y
 
-        if self.mode == 0:
+        # if self.mode == 0:
+        #     self.canvas.create_line(0,0, 100, 100, fill="yellow")
+
+        if self.mode == 1:
             # Draw a red dot at the clicked location
             if len(self.calibration) < 2:
-                self.canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill="red")
+                self.canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill="red", tags="calibration")
                 self.calibration.append((x,y))
+                if len(self.calibration) == 2:
+                    self.btn_ok.config(state="normal")
 
-            # Print the clicked coordinates
-            print(self.calibration)
 
-
-    def distance(self):
+    def distance(self, p1, p2):
         c = self.calibration
-        dx = c[0][0] - c[1][0]
-        dy = c[1][0] - c[1][1]
+        dx = p1[0] - p2[0]
+        dy = p1[0] - p2[1]
         return math.sqrt(dx**2 + dy**2)
 
 
